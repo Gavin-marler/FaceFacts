9e4fcd19e9338d2a7e437f323a0d29b3647ba3eb
Gavin Marler <gavin.marler17@gmail.com>
2024-01-23 20:46:53 -0600

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Gavin Marler on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
